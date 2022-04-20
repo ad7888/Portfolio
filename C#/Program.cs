418166ad16a0bc0b8f30edf939824f2fd3389488
ad7888 <ad7888-5@mail.ru>
2022-04-20 10:52:36 +0300

    Initial Commit

---
+++ C#/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _141
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Строка1";
+
+            string str2 = new string('s', 5); //объяснить синтаксис и назначение этого конструктора
+
+            char[] charray = {'t','e','s','t'};
+
+            string str3 = new string(charray);
+            string str4 = str2;
+            Console.WriteLine(str4);
+            if (str4==str2) Console.WriteLine(str3);
+            Console.WriteLine(str3[1]);
+            Console.WriteLine(str1+str2);
+
+        }
+    }
+}
